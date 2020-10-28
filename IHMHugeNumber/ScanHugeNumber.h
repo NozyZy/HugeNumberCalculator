2c04e65b250c585d4faa9349a02450822b6903ad
NozyZy <60878689+NozyZy@users.noreply.github.com>
2020-10-28 15:58:37 +0100

    Revert "Dev lot3"

--- IHMHugeNumber/ScanHugeNumber.h
+++
@@ -1,9 +0,0 @@
-#ifndef SCAN_HUGE_NUMBER
-#define SCAN_HUGE_NUMBER
-
-#include "../UseHugeNumber/Type/HugeInt.h"
-
-extern HugeInt*         scanHugeInt         (void);
-extern HugeUnsignedInt* scanHugeUnsignedInt (void);
-
-#endif // !SCAN_HUGE_NUMBER
