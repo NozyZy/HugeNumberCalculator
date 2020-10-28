1bbacebbec769a071a94bd1fbab325f36f7273f9
lisasanglar <63150772+lisasanglar@users.noreply.github.com>
2020-10-28 15:46:42 +0100

    Add files via upload

---
+++ UseHugeNumber/Operator/HugeFloatOperator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "HugeFloatOperator.h"
+
+HugeFloat* addHugeFloat (const HugeFloat* operand1, const HugeFloat* operand2) {
+    return NULL;
+}
+
+HugeFloat* substractHugeFloat (const HugeFloat* operand1, const HugeFloat* operand2) {
+    return NULL;
+}
+
+HugeFloat* multiplyHugeFloat (const HugeFloat* operand1, const HugeFloat* operand2) {
+    return NULL;
+}
+
+HugeFloat* simplifyHugeFloat (const HugeFloat* hugeFloat) {
+    return NULL;
+}
