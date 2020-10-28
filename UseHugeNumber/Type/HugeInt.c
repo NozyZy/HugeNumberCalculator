e14486e7d8b0c41e2491efb9e7d27e84b47c9ae1
Vallaire <61429521+Vallaire@users.noreply.github.com>
2020-10-28 16:18:03 +0100

    Add files via upload

---
+++ UseHugeNumber/Type/HugeInt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "HugeInt.h"
+
+HugeInt* createHugeInt (void) {
+    return NULL;
+}
+
+HugeInt* createHugeIntFromString (const char* digitString) {
+    return NULL;
+}
+
+HugeInt* createHugeIntFromHugeInt (const HugeInt* hugeInt, const unsigned int size) {
+    return NULL;
+}
+
+void deleteHugeInt (const HugeInt* hugeInt) {
+}
+
+int getHugeIntLength (const HugeInt* hugeInt) {
+    return 0;
+}
+
+HugeInt* simplifyHugeInt (const HugeInt* hugeInt) {
+    return NULL;
+}
