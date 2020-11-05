79e8c75b1855018eddd435c207261da52b7ceb27
Quentin V <quentin.vincent@efrei.net>
2020-11-05 10:20:20 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,18 +14,4 @@ int main (void) {
  hugeNumber = createHugeIntFromString
 ("-0000000123456789012345678901234567890");
  printHugeInt (hugeNumber);
- 
- hugeNumber = createHugeFloatFromString
-("123456789012345678901234567890.00123450000");
- printHugeFloat (hugeNumber);
- hugeNumber = createHugeFloatFromString
-("0000000123456789012345678901234567890.00123450000");
- printHugeFloat (hugeNumber);
- 
- hugeNumber = createHugeFloatFromString
-("-123456789012345678901234567890.00123450000");
- printHugeFloat (hugeNumber);
- hugeNumber = createHugeFloatFromString
-("-0000000123456789012345678901234567890.00123450000");
- printHugeFloat (hugeNumber);
 } 
