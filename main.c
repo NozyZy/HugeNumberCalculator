693041d37a74a3b1411d0fcb590376943d6fe756
Quent <quentin.vincent@efrei.net>
2020-11-05 11:10:22 +0100

    ShowHugeFloat :+1: fonctions en partie réparées

--- main.c
+++ main.c
@@ -1,17 +1,7 @@
 #include "..\IHMHugeNumber\ShowHugeNumber.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 int main (void) {
-    HugeFloat* hugeNumber = createHugeFloatFromString
-            ("00000000123456789012345678901234567890", "000012340000");
-    printHugeFloat (hugeNumber);
-    hugeNumber = createHugeFloatFromString
-            ("0000000123456789012345678901234567890", "-000012340000");
-    printHugeFloat (hugeNumber);
-
-    hugeNumber = createHugeFloatFromString
-            ("-00000000123456789012345678901234567890", "000012340000");
-    printHugeFloat (hugeNumber);
-    hugeNumber = createHugeFloatFromString
-            ("-0000000123456789012345678901234567890", "-000012340000");
-    printHugeFloat (hugeNumber);
+    printf("Hello World !\n");
 }
