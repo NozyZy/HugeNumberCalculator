1d6a9e3c3844df5169e278a63fcbf0b66a70cef0
Quentin V <quentin.vincent@efrei.net>
2020-11-05 10:39:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,5 @@
 #include "..\IHMHugeNumber\ShowHugeNumber.h"
 
 int main (void) {
- HugeInt* hugeNumber = createHugeIntFromString
-("123456789012345678901234567890");
- printHugeInt (hugeNumber);
- hugeNumber = createHugeIntFromString
-("0000000123456789012345678901234567890");
- printHugeInt (hugeNumber);
-
- hugeNumber = createHugeIntFromString
-("-123456789012345678901234567890");
- printHugeInt (hugeNumber);
- hugeNumber = createHugeIntFromString
-("-0000000123456789012345678901234567890");
- printHugeInt (hugeNumber);
+ printf("Helow World!\n");
 } 
