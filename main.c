76bab763fb65c3b222cb73b50e4adbb87960a4dc
Valere GOMEZ <valere.gomez@efrei.net>
2020-11-05 09:45:38 +0100

    pour les include

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include "..\IHMHugeNumber\ShowHugeNumber.h"
 
+#include "UseHugeNumber/Type/HugeUnsignedInt.h"
+
 int main (void) {
  HugeInt* hugeNumber = createHugeIntFromString
 ("123456789012345678901234567890");
